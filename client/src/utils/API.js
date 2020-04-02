23ef63d621439628474edff347d5ce92f4834289
Git Sadiq <abdulaisadiq@live.com>
2020-04-02 14:15:57 -0700

    Landing page updated

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,5 +1,6 @@
 const axios = require('axios');
 
 export default {
-    login: (data)=> axios.post('/api/login', data)
+    login: (data)=> axios.post('/api/login', data),
+    signUp: (data)=> axios.post('/api/signup', data)
 }
