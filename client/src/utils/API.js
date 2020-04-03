4b8eb82185b77e8d645aca5f3d8293b5e3476f1d
Armen M <mkhitaryanarmen@yahoo.com>
2020-04-03 14:16:23 -0700

    update

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -2,5 +2,6 @@ const axios = require('axios');
 
 export default {
     login: (data)=> axios.post('/data/login', data),
-    signUp: (data)=> axios.post('/data/signup', data)
+    signUp: (data)=> axios.post('/data/signup', data),
+    getHeadlines: ()=> axios.get('/api/news')
 }
