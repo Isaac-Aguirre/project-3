62ad441d587dd7bd6d548bfec863471abb3cf9b5
Isaac-Aguirre <isaac42700@gmail.com>
2020-04-07 12:03:24 -0700

    ok

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 //import './App.css';
 //import Navbar from './components/Navbar/Navbar';
 //import Sidebar from './components/Sidebar/Sidebar';
@@ -14,46 +14,42 @@ import {
 } from "react-router-dom";
 import API from './utils/API';
 import loggedContext from './utils/userContext';
-
 function App() {
-    const [user, setUser] = useState({user:null});
+    const [user, setUser] = useState({ user: null });
     const [followers, setFollowers] = useState([])
-
-    useEffect(()=>{
-        if(user){
+    useEffect(() => {
+        if (user) {
             navigator.geolocation.getCurrentPosition(
-                position=> console.log(position)
+                position => console.log(position)
             )
-        }else{
+        } else {
             console.log()
         }
-    },[user])
-
-    const handleLogin =(data)=>{
-        API.login(data).then(res=>{
+    }, [user])
+    const handleLogin = (data) => {
+        API.login(data).then(res => {
             console.log(res);
             setUser(res.data);
             // API.getFollowers(res.data.id).then(response=> setFollowers(JSON.parse(response.data)))
-        }).catch(err=>console.log(err))
+        }).catch(err => console.log(err))
     }
-
-
+    const handleSignup = (data) =>{
+        API.signUp(data).then(res=>{
+            console.log(res);
+            setUser(res.data);
+        })
+    }
     return (
         <div className='container'>
             <Router>
-
-            <loggedContext.Provider value={{...user, login: handleLogin, signUp: handleSignup}}>
-            
-
-                <Route exact path='/home' component={HomePage}/>
-                <Route exact path="/" component={LandingPage}/>
-                </loggedContext.Provider>
+                <Switch>
+                    <loggedContext.Provider value={{ ...user, login: handleLogin, signUp: handleSignup }}>
+                        <Route exact path='/home' component={HomePage} />
+                        <Route exact path="/" component={LandingPage} />
+                    </loggedContext.Provider>
                 </Switch>
-           
             </Router>
-                </Switch>
         </div>
     )
 }
-
 export default App;
